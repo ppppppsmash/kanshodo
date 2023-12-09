629f04cf2165efb1fae9764d2dea8dedb87393d0
ppppppsmash <pppppp.work@gmail.com>
2023-12-09 23:57:30 +0900

    chore: tailwindcss install

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1 class="text-3xl mt-10">Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
